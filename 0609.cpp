bc9ad9ede60a567db395784d190a1df3c1cae703
4D-13\koyo <a>
2026-06-09 13:38:52 +0900

    変更

--- 0609.cpp
+++ 0609.cpp
@@ -2,9 +2,32 @@
 //
 
 #include <iostream>
+#include <assert.h>
+
+class Test 
+{
+public:
+	void Show()
+	{
+		std::cout << "Hello World!!" << std::endl;
+	}
+
+
+
+};
+
+void Func(Test* test)
+{
+	assert(test != nullptr);
+	test->Show();
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	Func(new Test());
+	Func(nullptr);
+   
+
 }
 
