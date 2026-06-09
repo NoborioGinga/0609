d8aeb6f475cb2f4d34aa9812c6d6202bf5aa864b
4D-13\koyo <a>
2026-06-09 13:52:45 +0900

    保存

--- 0609.cpp
+++ 0609.cpp
@@ -4,30 +4,71 @@
 #include <iostream>
 #include <assert.h>
 
-class Test 
+//class Test 
+//{
+//public:
+//	void Show()
+//	{
+//		std::cout << "Hello World!!" << std::endl;
+//	}
+//
+//
+//
+//};
+//
+//void Func(Test* test)
+//{
+//	assert(test != nullptr);
+//	test->Show();
+//}
+//
+//int main()
+//{
+//
+//	Func(new Test());
+//	Func(nullptr);
+//   
+//
+//}
+
+
+
+
+class Weapon
 {
 public:
-	void Show()
-	{
-		std::cout << "Hello World!!" << std::endl;
-	}
 
+	void Use(){}
 
 
 };
 
-void Func(Test* test)
+class Player
 {
-	assert(test != nullptr);
-	test->Show();
-}
+	std::shared_ptr<Weapon> weapon;
+public:
+	Player(std::shared_ptr<Weapon> weapon1)
+		:weapon(std::move(weapon1)) {
+		std::cout << weapon.use_count() << std::endl;
+		std::cout << weapon1.use_count() << std::endl;
 
-int main()
+	}
+
+	void Attack()
+	{
+		if (weapon) weapon->Use();
+	
+	}
+
+
+};
+
+int main() 
 {
-
-	Func(new Test());
-	Func(nullptr);
-   
-
+	auto wp = std::make_shared<Weapon>();
+	//コピー
+	Player player1(wp);
+	//ムーブ
+	Player q(std::move(wp));
+		return 0;
 }
-
